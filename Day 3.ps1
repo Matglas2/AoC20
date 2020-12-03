4083abc391570b74fb041cf30d5016f3d7a5aa58
Erends <thijs.erends@maandag.nl>
2020-12-03 12:57:08 +0100

    Minor improvements

--- Day 3.ps1
+++ Day 3.ps1
@@ -6,14 +6,12 @@ function CircleJerk ($max, $current, $increment) {
 
 function TestPath ($path, $xstep, $ystep) {
     $forest = Get-Content $path                                    #Load to 2D array
-
     $x = 0; $y = 0; $count = 0;                                    #Set vars
     while ($y -le ($forest.Length -1)) {
         if ($forest[$y][$x] -eq "#") { $count++ }                  #Find tree at y.x location
         $x = CircleJerk ($forest[0].Length -1) $x $xstep           #Move cursor x
         $y += $ystep;                                              #Move cursor y
     }
-
     return $count
 }
 
